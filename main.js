2859082f24aad3119eabff33d611a10f11c75ef9
Antony Church <achurch44.ac@gmail.com>
2016-10-21 14:17:11 +0100

    Added Bootstrap CSS and Fonts, implemented basic UI elements.

--- main.js
+++ main.js
@@ -6,13 +6,10 @@ let win
 
     function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 300, height: 135})
 
     // and load the index.html of the app.
     win.loadURL(`file://${__dirname}/index.html`)
-
-		// Open the DevTools.
-		win.webContents.openDevTools()
 
 		// Emitted when the window is closed.
 		win.on('closed', () => {
@@ -47,3 +44,4 @@ let win
 
     // In this file you can include the rest of your app's specific main process
     // code. You can also put them in separate files and require them here.
+
